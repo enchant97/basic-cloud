7a414b3d1029b6dee665707932c49f5da5aef139
Leo Spratt <enchant97@users.noreply.github.com>
2021-04-13 19:52:57 +0100

    add file upload support on web

--- basic_cloud/static/script.js
+++ basic_cloud/static/script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const TOKEN_KEY = "token";
-
+var curr_dir;
 /**
  * redirect to the login page
  */
@@ -99,10 +99,12 @@ function remove_token() {
  * @returns html header for use with fetch
  */
 function get_auth_headers(content_type = "application/json") {
-    return {
+    const headers = {
         "Authorization": `Bearer ${get_stored_token()}`,
         "Content-Type": content_type,
-    }
+    };
+    if (content_type === null) { delete headers["Content-Type"]; }
+    return headers;
 }
 
 /**
@@ -183,6 +185,24 @@ async function fetch_download_token(file_path) {
 }
 
 /**
+ * upload a file
+ * @param {FormData} form_data - the file and directory to upload to
+ * @returns
+ */
+async function fetch_upload_file(form_data) {
+    const resp = await fetch("/api/file/upload/overwrite",
+        {
+            method: "POST",
+            body: form_data,
+            headers: get_auth_headers(null),
+        });
+    if (resp.status === 401) { navigate_to_login(); }
+    if (!resp.ok) { throw new Error(resp.status) }
+    const json_data = await resp.json();
+    return json_data;
+}
+
+/**
  * start downloading a file
  * @param {string} file_path - path of file to download
  */
@@ -194,6 +214,38 @@ function start_download_file(file_path) {
         a_elem.click();
         a_elem.remove();
     });
+}
+
+/**
+ * start uploading a file
+ * @param {string} root_path - the path to use for upload destination
+ * @param {string} file_elem - the file element to get file from
+ */
+async function start_upload_file(root_path, file_elem) {
+    const form_data = new FormData();
+    form_data.append("file", file_elem.files[0]);
+    form_data.append("directory", root_path);
+    await fetch_upload_file(form_data);
+    file_elem.remove();
+    alert("uploaded");
+}
+
+/**
+ * show the dialog for uploading a
+ * new file to current directory and then upload it
+ */
+function upload_file() {
+    if (curr_dir) {
+        const file_elem = document.createElement("input");
+        file_elem.setAttribute("type", "file");
+        file_elem.addEventListener("input", _ => {
+            start_upload_file(curr_dir, file_elem);
+        });
+        file_elem.click();
+    }
+    else {
+        alert("cannot upload here...");
+    }
 }
 
 /**
@@ -236,6 +288,8 @@ async function load_roots() {
     append_path_row(folders_elem, roots.shared, roots.shared, true);
     append_path_row(folders_elem, roots.home, roots.home, true);
     update_curr_dir_status("");
+    curr_dir = null;
+    document.getElementById("upload-file-bnt").setAttribute("disabled", true);
 }
 
 /**
@@ -263,6 +317,8 @@ async function change_directory(new_directory) {
             append_path_row(files_elem, combined_path, path_name, false);
         }
     });
+    curr_dir = new_directory;
+    document.getElementById("upload-file-bnt").removeAttribute("disabled");
     update_curr_dir_status(new_directory.replace("\\", "/"));
 }
 
