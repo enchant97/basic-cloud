cae2914c8e558fa6ed7db471079b4e28f60c1e84
Leo Spratt <enchant97@users.noreply.github.com>
2021-04-13 20:29:25 +0100

    add metadata for fastapi docs

--- basic_cloud/main.py
+++ basic_cloud/main.py
@@ -6,14 +6,37 @@ from .database import models
 from .helpers.constants import STATIC
 from .router import auth, folder, html, users, file
 
-app = FastAPI()
+tags_metadata = (
+    {
+        "name": "token",
+        "description": "operations with user authentication tokens"
+    },
+    {
+        "name": "users",
+        "description": "operations with users"
+    },
+    {
+        "name": "directories",
+        "description": "operations with directories"
+    },
+    {
+        "name": "files",
+        "description": "operations with files"
+    },
+    {
+        "name": "html",
+        "description": "html pages to view"
+    },
+)
+
+app = FastAPI(openapi_tags=tags_metadata)
 app.mount("/static", STATIC, name="static")
 
 app.include_router(html.router, tags=["html"])
-app.include_router(auth.router)
-app.include_router(users.router, prefix="/api/users")
-app.include_router(folder.router, prefix="/api/directory")
-app.include_router(file.router, prefix="/api/file")
+app.include_router(auth.router, tags=["token"])
+app.include_router(users.router, prefix="/api/users", tags=["users"])
+app.include_router(folder.router, prefix="/api/directory", tags=["directories"])
+app.include_router(file.router, prefix="/api/file", tags=["files"])
 
 
 @app.on_event("startup")
